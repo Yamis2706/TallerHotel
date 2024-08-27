e96f89924d91a7c62aa4418a27b51aab8ed7a6f5
Yamileth Londoño Burgos <142650446+Yamis2706@users.noreply.github.com>
2024-08-27 16:15:22 -0500

    Desarrollo
    Desarrollo del proyecto

--- co.edu.uniquindio.tallerHotel/src/main/java/co/edu/uniquindio/tallerHotel/model/Hotel.java
+++ co.edu.uniquindio.tallerHotel/src/main/java/co/edu/uniquindio/tallerHotel/model/Hotel.java
@@ -1,4 +1,48 @@
 package co.edu.uniquindio.tallerHotel.model;
 
-public class Hotel {
+import co.edu.uniquindio.tallerHotel.model.constante.TamanoMatriz;
+import co.edu.uniquindio.tallerHotel.model.enums.EstadoReserva;
+import co.edu.uniquindio.tallerHotel.model.enums.TipoHabitacion;
+import co.edu.uniquindio.tallerHotel.model.service.ServicioHabitacion;
+import lombok .Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.UUID;
+
+/** Esta clase representa un Hotel con sus habitaciones, reservas y clientes
+ * asociados.
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+public class Hotel implements ServicioHabitacion {
+
+    private final Habitacion[][] habitaciones;
+    private final ArrayList<Reserva> reservas;
+    private final ArrayList<Cliente> clientes;
+
+
+    public Hotel(){
+        this.habitaciones = new Habitacion[TamanoMatriz.FILAS][TamanoMatriz.COLUMNAS];
+        this.reservas = new ArrayList<>();
+        this.clientes = new ArrayList<>();
+        inicializarHabitaciones();
+    }
+
+
+    /** Método que inicializa las habitaciones con valores por defecto y los
+     * almacena en la matriz de habitaciones.
+     */
+
+    private void inicializarHabitaciones(){
+        for (int i = 0; i < habitaciones[0].length; i++){
+            habitaciones[0][i] = new Habitacion(150)
+        }
+    }
+
+
 }
---
+++ co.edu.uniquindio.tallerHotel/src/main/java/co/edu/uniquindio/tallerHotel/model/constante/TamanoMatriz.java
@@ -0,0 +1,6 @@
+package co.edu.uniquindio.tallerHotel.model.constante;
+
+public class TamanoMatriz {
+     public static final int FILAS = 5;
+     public static final int COLUMNAS = 5;
+}
